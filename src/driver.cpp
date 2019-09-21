132aff317eb5ce3d985f3547586ebc2e47c75317
jadon <jadonwolffs@gmail.com>
2019-09-21 12:22:41 +0200

    added include header and main

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,2 +1,7 @@
 // TEMPLATE AUTHOR: Jadon Wolffs
 // CLONE TEMPLATE with: git clone https://github.com/jadonwolffs/cpp_templates.git
+#include "header.h"
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
